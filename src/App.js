91677466afae930a14bc8c63f22f712e56068bdd
AdityaTohan <tohanaditya@gmail.com>
2022-03-25 17:50:18 +0530

    new

--- src/App.js
+++ src/App.js
@@ -8,99 +8,85 @@ import htmlToPdfmake from 'html-to-pdfmake';
 class App extends React.Component {
 
   constructor(props) {
-    super(props);
-    this.state={
-      user:[],
-      isLoading:false,
-      isError:false
+    super(props)
+    this.state = {
+      users: [],
+      isLoading: false,
+      isError: false
     }
   }
 
-  //api calling
-  async componentDidMount(){
-    this.setState({isLoading:true})
 
-    const response = await fetch("https://jasonplaceholder.typicode.com/users")
-
-    if(response.ok) {
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    if (response.ok) {
       const users = await response.json()
-      console.log(users)
-      this.state({users, isLoading: false})
-    }
-    else{
-      this.setState({isError:true,isLoading:false})
+      this.setState({ users, isLoading: false })
+    } else {
+      this.setState({ isError: true, isLoading: false })
     }
   }
-
-  printDocument() {
-
-    const doc = new jsPDF();
-
-    const pdfTable = document.getElementById('divToPrint');
-    var html = htmlToPdfmake(pdfTable.innerHTML);
-
-    const documentDefinition = { content: html };
-    pdfMake.vfs = pdfFonts.pdfMake.vfs;
-    pdfMake.createPdf(documentDefinition).open();
-
-  }
-
   renderTableHeader = () => {
-    return Object.keys(this.state.users[0]).map(attr => <th key={attr}>
-
-        </th>)
+    return Object.keys(this.state.users[0]).map(attr => <th key={attr}>{attr}</th>)
   }
-
   renderTableRows = () => {
     return this.state.users.map(user => {
-      return(
+      return (
           <tr key={user.id}>
             <td>{user.id}</td>
             <td>{user.name}</td>
+            <td>{user.username}</td>
             <td>{user.email}</td>
           </tr>
       )
     })
   }
+    printDocument() {
 
-  render() {
+        const doc = new jsPDF();
 
-    const{users,isLoading,isError}=this.state
+        const pdfTable = document.getElementById('divToPrint');
+        var html = htmlToPdfmake(pdfTable.innerHTML);
 
-    if(isLoading){
-      return <div>Loading..</div>
+        const documentDefinition = { content: html };
+        pdfMake.vfs = pdfFonts.pdfMake.vfs;
+        pdfMake.createPdf(documentDefinition).open();
+
     }
 
-    if(isError){
-      return <div>Error...</div>
+
+    render() {
+    const { users, isLoading, isError } = this.state
+
+    if (isLoading) {
+      return <div>Loading...</div>
     }
 
+    if (isError) {
+      return <div>Error</div>
+    }
 
-    return (
-        <div className="App container mt-5">
-
-          <div id="divToPrint">
-            <h2>Table to PDF</h2>
-
-            <table className="table">
-              <thead className="thead-dark">
+    return users.length > 0
+        ? (
+            <table>
+              <thead>
               <tr>
                 {this.renderTableHeader()}
               </tr>
               </thead>
               <tbody>
-              <tr>
-                {this.renderTableRows()}
-              </tr>
+              {this.renderTableRows()}
               </tbody>
             </table>
 
-          </div>
+        ) : (
+            <div>
+              No users.
+            </div>
+        )
 
-          <button class="btn btn-primary float-right" onClick={this.printDocument}>Export To PDF</button>
-        </div>
-    )
-  };
+  }
 }
 
 
